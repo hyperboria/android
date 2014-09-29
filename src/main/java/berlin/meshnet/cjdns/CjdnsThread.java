4e1c34fa7559910b4a85539493090a2dd7292ba3
Lars Gierth <larsg@systemli.org>
2014-09-29 06:33:25 +0200

    Start cjdroute, with abort for now

--- src/main/java/berlin/meshnet/cjdns/CjdnsThread.java
+++ src/main/java/berlin/meshnet/cjdns/CjdnsThread.java
@@ -3,21 +3,58 @@ package berlin.meshnet.cjdns;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class CjdnsThread implements Runnable {
     private File cjdroute;
+    private InputStream cjdrouteconf;
     private TextView logView;
 
-    public CjdnsThread(File cjdroute, TextView logView)
+    public CjdnsThread(File cjdroute, InputStream cjdrouteconf, TextView logView)
     {
         this.cjdroute = cjdroute;
+        this.cjdrouteconf = cjdrouteconf;
         this.logView = logView;
     }
 
     @Override
     public void run()
     {
-        logView.setText("CjdnsThread: " + cjdroute.getPath());
+        ProcessBuilder builder = new ProcessBuilder(cjdroute.getPath());
+        builder.redirectErrorStream(true);
+
+        try {
+            Process process = builder.start();
+
+            OutputStream stdin = process.getOutputStream();
+            byte buf[] = new byte[4096];
+
+            int len = cjdrouteconf.read(buf);
+            while (len > 0) {
+                stdin.write(buf, 0, len);
+                len = cjdrouteconf.read(buf);
+            }
+            stdin.close();
+
+            InputStreamReader reader = new InputStreamReader(process.getInputStream());
+            BufferedReader stdout = new BufferedReader(reader);
+
+            StringBuilder log = new StringBuilder();
+
+            while (true) {
+                String line = stdout.readLine();
+                if (line == null)
+                    return;
+
+                logView.setText(log.append(line + "\n"));
+            }
+        } catch (IOException e) {
+            Log.e("cjdns_CjdnsThread", "IOException: " + e.getMessage());
+        }
     }
 }
--- src/main/java/berlin/meshnet/cjdns/MainActivity.java
+++ src/main/java/berlin/meshnet/cjdns/MainActivity.java
@@ -18,51 +18,54 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        writeCjdroute();
-        startCjdnsThread((TextView)findViewById(R.id.hello));
+        try {
+            writeCjdroute();
+            startCjdnsThread((TextView)findViewById(R.id.hello));
+        } catch (IOException e) {
+            Log.e("cjdns_MainActivity", "IOException: " + e.getMessage());
+        }
     }
 
-    private void startCjdnsThread(TextView logView)
+    private void startCjdnsThread(TextView logView) throws IOException
     {
-        Thread cjdns = new Thread(new CjdnsThread(cjdroute(), logView), "CjdnsThread");
+        Thread cjdns = new Thread(new CjdnsThread(cjdroute(), cjdrouteconf(), logView), "CjdnsThread");
         cjdns.start();
     }
 
-    private boolean writeCjdroute()
+    private void writeCjdroute() throws IOException
     {
         File target = cjdroute();
         if (target.exists() && target.canExecute()) {
             Log.i("cjdns_MainActivity", "cjdroute exists and is executable");
-            return true;
+            return;
         }
 
-        try {
-            InputStream asset = getAssets().open("cjdroute");
-            FileOutputStream cjdroute = new FileOutputStream(target);
-            byte buf[] = new byte[4096];
+        InputStream asset = getAssets().open("cjdroute");
+        FileOutputStream cjdroute = new FileOutputStream(target);
+        byte buf[] = new byte[4096];
 
-            int len = asset.read(buf);
-            while (len > 0) {
-                cjdroute.write(buf, 0, len);
-                len = asset.read(buf);
-            }
-            cjdroute.close();
+        int len = asset.read(buf);
+        while (len > 0) {
+            cjdroute.write(buf, 0, len);
+            len = asset.read(buf);
+        }
+        cjdroute.close();
 
-            if (!target.setExecutable(true)) {
-                Log.e("cjdns_MainActivity", "setExecutable() failed");
-                return false;
-            }
-        } catch (IOException e) {
-            Log.e("cjdns_MainActivity", "IOException: " + e.getMessage());
-            return false;
+        if (!target.setExecutable(true)) {
+            Log.e("cjdns_MainActivity", "setExecutable() failed");
+            return;
         }
 
         Log.i("cjdns_MainActivity", "Created cjdroute and made it executable");
-        return true;
     }
 
     private File cjdroute()
     {
         return new File(getCacheDir(), "cjdroute");
+    }
+
+    private InputStream cjdrouteconf() throws IOException
+    {
+        return getAssets().open("cjdroute.conf");
     }
 }
