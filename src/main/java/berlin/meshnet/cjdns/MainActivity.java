d171ba0bc221c5efc508506f463abec0d0aeffb9
Lars Gierth <larsg@systemli.org>
2014-09-29 02:54:15 +0200

    Create cjdroute executable from assets

--- src/main/java/berlin/meshnet/cjdns/MainActivity.java
+++ src/main/java/berlin/meshnet/cjdns/MainActivity.java
@@ -2,6 +2,12 @@ package berlin.meshnet.cjdns;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +17,40 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        writeCjdroute();
+    }
+
+    private boolean writeCjdroute()
+    {
+        File target = new File(getCacheDir(), "cjdroute");
+        if (target.exists() && target.canExecute()) {
+            Log.i("cjdns_MainActivity", "cjdroute exists and is executable");
+            return true;
+        }
+
+        try {
+            InputStream asset = getAssets().open("cjdroute");
+            FileOutputStream cjdroute = new FileOutputStream(target);
+            byte buf[] = new byte[4096];
+
+            int len = asset.read(buf);
+            while (len > 0) {
+                cjdroute.write(buf, 0, len);
+                len = asset.read(buf);
+            }
+            cjdroute.close();
+
+            if (!target.setExecutable(true)) {
+                Log.e("cjdns_MainActivity", "setExecutable() failed");
+                return false;
+            }
+        } catch (IOException e) {
+            Log.e("cjdns_MainActivity", "IOException: " + e.getMessage());
+            return false;
+        }
+
+        Log.i("cjdns_MainActivity", "Created cjdroute and made it executable");
+        return true;
     }
 }
