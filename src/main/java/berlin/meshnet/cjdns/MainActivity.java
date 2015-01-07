06093a76bee1c2b12737ca8c7cbf96a4b5792da8
Benedict Lau <ben.hy.lau@gmail.com>
2015-01-06 21:53:32 -0500

    Upgrading to Gradle 2.2.1

--- src/main/java/berlin/meshnet/cjdns/MainActivity.java
+++ src/main/java/berlin/meshnet/cjdns/MainActivity.java
@@ -3,6 +3,8 @@ package berlin.meshnet.cjdns;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity
 {
