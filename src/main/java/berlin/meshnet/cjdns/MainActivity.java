d94c7df88ef5ee113e55560326603c2435b9194f
Benedict Lau <ben.hy.lau@gmail.com>
2015-01-08 01:16:17 -0500

    Added actionbar and navigation drawer

--- src/main/java/berlin/meshnet/cjdns/MainActivity.java
+++ src/main/java/berlin/meshnet/cjdns/MainActivity.java
@@ -1,26 +1,101 @@
 package berlin.meshnet.cjdns;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
-{
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @InjectView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @InjectView(R.id.drawer)
+    ListView mDrawer;
+
+    @InjectView(R.id.content_container)
+    FrameLayout mContentContainer;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setSupportActionBar(mToolbar);
 
-        startService(new Intent(this, MeshnetService.class));
+        final ArrayAdapter<String> drawerOptions = new ArrayAdapter<String>(this, R.layout.view_drawer_option, getResources().getStringArray(R.array.drawer_options));
+        mDrawer.setAdapter(drawerOptions);
+        mDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mDrawerLayout.closeDrawer(Gravity.START);
+                Toast.makeText(getApplicationContext(), drawerOptions.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open_content_desc, R.string.drawer_close_content_desc) {
+            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                supportInvalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.actions, menu);
+        inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.toggle_cjdns) {
+            Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            startService(new Intent(this, MeshnetService.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
